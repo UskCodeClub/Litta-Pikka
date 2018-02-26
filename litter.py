88fa0535add4fc339350de9ab24090edc99daac9
Usk Code Club <teuluhowells5@gmail.com>
2018-02-26 17:16:57 +0000

    all files

---
+++ litter.py
@@ -0,0 +1,91 @@
+# User Interface
+from guizero import *
+from datetime import datetime
+
+#Database
+import sqlite3
+dbfile = sqlite3.connect('litter.db')
+db = dbfile.cursor()
+db.execute('''CREATE TABLE IF NOT EXISTS litter (date text, house text, weight real)''')
+
+# Twitter
+from twython import Twython
+app_key = "gtqeYPlONqKyZaJ4jWaFQw9vW"
+app_secret = "8PrhMwJ87zgsOS9vQKd7yK2AZNO9o2wJHVKNfsWhSw8nEAX8Az"
+oauth_token = "963470498884710400-hD2xP20nilT2oFDU00oieLfrNU7mZ2J"
+oauth_token_secret = "UYn0acDoQTTQfniyuIpG1qmO6hQ8MQZgPyVIFf65xmaES"
+twitter = Twython(app_key, app_secret, oauth_token, oauth_token_secret)
+
+thetime = datetime.now().strftime('%-I:%M%P on %d-%m-%Y')
+
+
+# Global Variables
+house = ''
+weight = ''
+#date = ''
+        
+# Add To Litter
+def enter_litter():
+
+    global house
+    global weight
+
+    weight = tb.value
+
+    if weight == '':
+        print ("You haven't entered a weight")
+    elif house == '':
+        print ("You haven't tapped a house icon")
+    else :
+        print ("Well done " +house)
+        twitter.update_status(status=house +" have added more litter. That's 5 Dojos for " + house + thetime)
+        print ("Adding "+weight + "g of litter to " +house)
+        q = "INSERT INTO litter VALUES (date('now'),'"
+        q += house
+        q += "', '"
+        q += weight
+        q += "')"
+        db.execute(q)
+        dbfile.commit()
+
+
+# Button Commands
+
+def add_wentwood():
+        global house
+        house = 'Wentwood'
+
+def add_snowdon():
+        global house
+        house = 'Snowdon'
+
+def add_elan():
+        global house
+        house = 'Elan'
+
+def add_gower():
+        global house
+        house = 'Gower'
+
+#Build User Interface
+app = App(title="LittaPikka", height=600, width=800)
+t01 = Text(app, text="Please select a house icon, then enter the weight of litter", font="20", grid=[0,5,5,1])
+box = Box(app, layout="grid")
+
+t02 = Text(box, text="Wentwood Wolves", font="20", grid=[1,1])
+t03 = Text(box, text="Snowdon Eagles", font="20", grid=[4,1])
+
+b1 = PushButton (box, command=add_wentwood, icon="Wentwood.gif", grid=[1,2])
+b2 = PushButton (box, command=add_snowdon, icon="Snowdon.gif", grid=[4,2])
+b3 = PushButton (box, command=add_elan, icon="Elan.gif", grid=[1,3])
+b4 = PushButton (box, command=add_gower, icon="Gower.gif", grid=[4,3])
+
+t05 = Text(box, text="Elan Kites", font="20", grid=[1,4])
+t06 = Text(box, text="Gower Dragons", font="20", grid=[4,4])
+t07 = Text(app, text="Enter weight of litter in grams", grid=[5,1,5,1])
+
+tb = TextBox(app)
+button = PushButton(app, enter_litter, text="Enter litter")
+
+app.display()
+
